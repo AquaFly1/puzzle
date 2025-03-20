51135a5101fbc3f3460701a5280011a0b9c80386
AquaFly1 <louis.emery@hotmail.com>
2025-03-20 13:47:41 -0400

    asd

--- new-game-project/scripts/mec.gd
+++ new-game-project/scripts/mec.gd
@@ -39,22 +39,64 @@ func _input(event: InputEvent) -> void:
 		offset = Vector2i(1,0)
 		
 	if player_pos != next_player_pos:
+		var id = tile_map_layer_2.get_cell_source_id(next_player_pos)
+		print(id)
 		next_player_pos = _teleport(next_player_pos)
+		if id == 5 or id == 6:
+			print("look left")
+			offset = Vector2i(-1,0)
+			player.rotation = 3*PI/2
+		elif id == 3 or id == 8:
+			print("look right")
+			offset = Vector2i(1,0)
+			player.rotation = PI/2
+		elif id == 2 or id == 7:
+			print("look up")
+			offset = Vector2i(0,-1)
+			player.rotation = 0
+		elif id == 4 or id == 9:
+			print("look down")
+			offset = Vector2i(0,1)
+			player.rotation = PI
+		
 		can_teleport = true
 	if _is_valid(next_player_pos):
 		player_pos = next_player_pos
 		
 	if Input.is_action_just_pressed("blue"):
-		for i in tile_map_layer_2.get_used_cells_by_id(6, Vector2i(0,0)):
-			tile_map_layer_2.erase_cell(i)
+		var id =0
+		var ids = [2,3,4,6]
+		for x in ids:
+			for i in tile_map_layer_2.get_used_cells_by_id(x, Vector2i(0,0)):
+				tile_map_layer_2.erase_cell(i)
 		portals[0][0] = _find_wall()
-		tile_map_layer_2.set_cell(_find_wall(), 6, Vector2i(0,0))
+		if offset == Vector2i(1,0): #right
+			id = 6
+		if offset == Vector2i(-1,0): #left
+			id = 3
+		if offset == Vector2i(0,1): #down
+			id = 2
+		if offset == Vector2i(0,-1): #up
+			id = 4
+		tile_map_layer_2.set_cell(_find_wall(), id, Vector2i(0,0))
 		
 	if Input.is_action_just_pressed("red"):
-		for i in tile_map_layer_2.get_used_cells_by_id(5, Vector2i(0,0)):
-			tile_map_layer_2.erase_cell(i)
+		var id = 0
+		var ids = [5,7,8,9]
+		for x in ids:
+			for i in tile_map_layer_2.get_used_cells_by_id(x, Vector2i(0,0)):
+				tile_map_layer_2.erase_cell(i)
+			
 		portals[0][1] = _find_wall()
-		tile_map_layer_2.set_cell(_find_wall(), 5, Vector2i(0,0))
+		if offset == Vector2i(1,0): #right
+			id = 5
+		if offset == Vector2i(-1,0): #left
+			id = 8
+		if offset == Vector2i(0,1): #down
+			id = 7
+		if offset == Vector2i(0,-1): #up
+			id = 9
+		tile_map_layer_2.set_cell(_find_wall(), id, Vector2i(0,0))
 
 func _is_valid(pos):
 	return pos not in tile_map_layer_2.get_used_cells_by_id(0, Vector2i(0,0))
